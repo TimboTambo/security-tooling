e65202d5592c25d0ddf487ff1d43b4bd6e3653b0
Tim Lee <tim.lee@red-badger.com>
2021-04-26 14:40:28 +0100

    Add eslint plugins and introduce some security issues

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next'
+import { promises as fs } from 'fs'
+import { pseudoRandomBytes } from 'crypto'
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Dome' })
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  res.status(200).json({ name: req.body })
+
   console.warn('-----BEGIN PRIVATE KEY-----')
+
+  eval(req.body)
+  res.setHeader('X-POWERED-BY', 'My vulnerable technology')
+
+  await fs.readFile(req.body)
+
+  const badRegex = /^(([a-z])+.)+[A-Z]([a-z])+$/
+  badRegex.exec(req.body)
+
+  pseudoRandomBytes(256, req.body)
 }
 
 export default handler
