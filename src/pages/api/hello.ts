964729b4245899a3905b6f0ff5cd04a87e3f8064
Tim Lee <tim.lee@red-badger.com>
2021-04-26 11:37:04 +0100

    Expected CI failure by introducing private key string

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -4,6 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: 'John Doe' })
+  console.warn('-----BEGIN PRIVATE KEY-----')
 }
 
 export default handler
